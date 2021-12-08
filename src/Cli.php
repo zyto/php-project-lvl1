6969ee00ea5d4c69b91955e360858fbb818707a8
zyto <zytov@yandex.ru>
2021-12-08 06:23:14 -0800

    Fix test error on Cli.php v2

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function welcome()
 {
-line("Welcome to the Brain Games!");
+    line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, {$name}!");
     //
